089592106c78f71b21c8326728238fcf7eba17c5
arnabshi <arnabshigaming1712@gmail.com>
2023-04-16 12:26:43 +0530

    blogs-Arnab(1st commit)

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,34 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        int indOfX = screenDimensions.indexOf('X');
 
+        int screenH = Integer.parseInt(screenDimensions.substring(0,indOfX));
+        int screenW = Integer.parseInt(screenDimensions.substring(indOfX+1));
+
+        Image image = imageRepository2.findById(id).get();
+
+        int indxOfxInImage = image.getDimensions().indexOf('X');
+
+        int imageH = Integer.parseInt(image.getDimensions().substring(0,indxOfxInImage));
+        int imageW = Integer.parseInt(image.getDimensions().substring(indxOfxInImage+1));
+
+        return (screenH/imageH) * (screenW/imageW);
     }
 }
